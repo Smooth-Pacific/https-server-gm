c5d0b7e3a408a140ee9c26632c380b9bf17ecf49
gjmistkawi <gjmistkawi@gmail.com>
2022-02-22 04:17:57 -0800

    JTN-59/60 features: Added threading capability to the server via the libhttpserver library

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,13 @@ int main() {
     httpserver::webserver ws = httpserver::create_webserver(8001)
         .use_ssl()
         .https_mem_key("/ca/server-ca/private/server-ca.key")
-        .https_mem_cert("/ca/server-ca/server-ca.crt");
+        .https_mem_cert("/ca/server-ca/server-ca.crt")
+        .max_connections(4)
+        .connection_timeout(100)
+        .memory_limit(30000)
+        .use_dual_stack() //could check fo ipv4/ipv6 here
+        .start_method(httpserver::http::http_utils::INTERNAL_SELECT)
+        .max_threads(8);
 
     hello_world_resource hwr;
     ws.register_resource("/hello", &hwr);
